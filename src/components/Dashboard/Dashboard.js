63e14f2dd5eb1871bd592f601c7a761258db859b
Ronnie Alsop <alsop.arron@gmail.com>
2025-04-06 22:00:08 -0700

    'wip'

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Dashboard.css';
 
 export default function Dashboard() {
   return (
-    <div style={{ color: 'white', padding: '2rem' }}>
-      <h1>Welcome, Blogger!</h1>
-      <p>This is your dashboard.</p>
+    <div className="dashboard-container">
+        <div><button className="logout-button">LOGOUT</button></div>
+        <div className="recipe-maker-entrance"><button>
+            Enter New Recipe
+        </button></div>
+        
     </div>
   );
 }
